0bf20014646f45bc6cbee37611e6a3f1212e5e23
20161104615 <ys1019172727@vip.qq.com>
2020-02-22 13:08:26 +0800

    多文件上传3.0

--- music-day01/src/main/java/com/ys/demo/controller/FilesController.java
+++ music-day01/src/main/java/com/ys/demo/controller/FilesController.java
@@ -1,16 +1,21 @@
 package com.ys.demo.controller;
 
+import com.ys.demo.bean.MusicBean;
 import com.ys.demo.service.MusicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /*
  * @Author 20161104615
@@ -19,6 +24,7 @@ import java.util.List;
  * @Param
  * @return
  **/
+@WebServlet
 @Controller
 @RequestMapping("/files")
 public class FilesController {
@@ -33,14 +39,17 @@ public class FilesController {
 
     @PostMapping(value = "/fileUpload")
     @ResponseBody
-    public String fileUpload(@RequestParam(value = "newMusicUrl") MultipartFile newMusicUrl,
-                             @RequestParam(value = "newMusicImgUrl") MultipartFile newMusicImgUrl,
-                             @RequestParam(value = "newMusicName") String newMusicName,
-                             @RequestParam(value = "newMusicSinger") String newMusicSinger,
-                             HttpServletRequest request) throws IOException {
+    public void fileUpload(@RequestParam(value = "newMusicUrl") MultipartFile newMusicUrl,
+                           @RequestParam(value = "newMusicImgUrl") MultipartFile newMusicImgUrl,
+                           @RequestParam(value = "newMusicName") String newMusicName,
+                           @RequestParam(value = "newMusicSinger") String newMusicSinger,
+                           HttpServletResponse response,
+                           HttpServletRequest request) throws IOException {
         System.out.println("进入filesupload");
+        request.setCharacterEncoding("utf-8");
+        response.setContentType("text/html;charset=utf-8");
         if (newMusicUrl.isEmpty() && newMusicImgUrl.isEmpty()) {
-            return "上传失败1";
+            response.sendRedirect("/music/fileupload.html");
         }
         String fileName = "";//文件名称
         String suffixName = "";//文件后缀名称
@@ -70,7 +79,7 @@ public class FilesController {
             } else if (".jpg".equals(suffixName) || ".png".equals(suffixName)) {
                 System.out.println("进入图片处理");
                 String filePath_newMusicImgUrl = "D:/JavaProgram/Apache-tomcat/apache-tomcat-8.5.43/webapps/ROOT/media/img/";//图片存放的路径
-                newNameImg = newMusicName+"-"+newMusicSinger+".jpg";//统一处理为.jpg
+                newNameImg = newMusicName + "-" + newMusicSinger + ".jpg";//统一处理为.jpg
                 /*File file_music_img = new File(filePath_newMusicImgUrl + fileName);*/
                 File file_music_img = new File(filePath_newMusicImgUrl + newNameImg);
                 try {
@@ -86,10 +95,22 @@ public class FilesController {
         boolean checkUpload = musicService.uploadMusic(newMusicName, newMusicSinger);
         if (checkUpload == true) {
             System.out.println("上传数据库成功");
+            Map<String, ArrayList<MusicBean>> musicByName = musicService.findMusicByName(newMusicName);
+            for (Map.Entry<String, ArrayList<MusicBean>> entry : musicByName.entrySet()) {
+                System.out.println("key= " + entry.getKey() + " and value= " + entry.getValue());
+                if (entry.getKey().toString() == "TURE") {
+                    //表示取值成功
+                    request.getSession().setAttribute("searchResult", entry.getValue().toArray());
+                    response.sendRedirect("/music/list.html");
+                } else {
+                    //表示未查询到
+                    request.getSession().setAttribute("searchResult", "FALSE");
+                    response.sendRedirect("/music/fileupload.html");
+                }
+            }
         } else {
             System.out.println("上传数据库失败");
-            return "error";
+            response.sendRedirect("/music/fileupload.html");
         }
-        return "success";
     }
 }
